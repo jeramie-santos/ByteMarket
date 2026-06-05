ee1c89bb989f377fbaa38dcef08a9e9703448c7c
Jeramie <jeramieramsantos79@gmail.com>
2026-06-05 11:19:12 +0800

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use(express());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in PORT ${PORT}`);
+})
