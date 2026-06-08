78702dd03c73be4acd8813ba8236f4dc63f17ec2
Jeramie <jeramieramsantos79@gmail.com>
2026-06-08 14:30:14 +0800

    updated navbar for links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
+
+  const { items } = useSelector((state) => state.carts);
+
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
+
   return (
-    <div className="border-2 py-8 px-4">
-        <h1>ByteMarket</h1>
-    </div>
+    <header className="flex justify-between border-2 py-8 px-4">
+        <div>
+          <Link to="/">
+            <strong>ByteMarket</strong>
+          </Link>
+        </div>
+        <div className="flex gap-2">
+          <Link to="/">Shop</Link>
+          <Link to="/cart">Cart ({totalItems})</Link>
+        </div>
+    </header>
   )
 }
 
