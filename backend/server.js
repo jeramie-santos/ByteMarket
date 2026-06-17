748c898b953422cd16c5d70224f9780d8d2074b2
Jeramie <jeramieramsantos79@gmail.com>
2026-06-17 11:02:24 +0800

    fix ping placement

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ require('dotenv').config();
 app.use(cors());
 app.use(express());
 
-app.use('/', productRoutes);
-
 app.get('/ping', (req, res) => {
   res.status(200).send('Server is awake!');
 });
+
+app.use('/', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 
